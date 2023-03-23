7afd99c238e83c3f5e57bbc358c3d4779b1f9ce7
MohabsMamdouh <mohabmamdouh22@gmail.com>
2023-03-23 08:12:05 +0200

    store location

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -7,108 +7,14 @@ use App\Http\Requests\StoreAddressRequest;
 use App\Http\Requests\UpdateAddressRequest;
 use Illuminate\Http\Request;
 use Stevebauman\Location\Facades\Location;
+use Illuminate\Support\Facades\Auth;
 
 
 class AddressController extends Controller
 {
-    private $countries = array(
-        array('AR', 'Argentina'),
-        array('AU','Australia'),
-        array('AT','Austria'),
-        array('BH', 'Bahrain'),
-        array('BY', 'Belarus'),
-        array('BE', 'Belgium'),
-        array('BZ','Belize' ),
-        array('BR', 'Brazil'),
-        array('bn', 'Brunei'),
-        array('bg', 'Bulgaria'),
-        array('ca', 'Canada'),
-        array('cl', 'Chile'),
-        array('cn', 'China'),
-        array('co', 'Colombia'),
-        array('cr', 'Costa Rica'),
-        array('hr', 'Croatia'),
-        array('cy', 'Cyprus'),
-        array('cz', 'Czech Republic'),
-        array('dk', 'Denmark'),
-        array('do', 'Dominican Republic'),
-        array('ec', 'Ecuador'),
-        array('eg', 'Egypt'),
-        array('sv', 'El Salvador'),
-        array('ee', 'Estonia'),
-        array('fi', 'Finland'),
-        array('fr', 'France'),
-        array('ge', 'Georgia'),
-        array('de', 'Germany'),
-        array('gr', 'Greece'),
-        array('gu', 'Guam'),
-        array('gt', 'Guatemala'),
-        array('hn', 'Honduras'),
-        array('hk', 'Hong Kong SAR'),
-        array('hu', 'Hungary'),
-        array('in', 'India'),
-        array('id', 'Indonesia'),
-        array('ie', 'Ireland'),
-        array('im', 'Isle of Man'),
-        array('il', 'Israel'),
-        array('it', 'Italy'),
-        array('jp', 'Japan'),
-        array('je', 'Jersey'),
-        array('kr', 'Korea'),
-        array('kg', 'Kyrgyzstan'),
-        array('lb', 'Lebanon'),
-        array('lt', 'Lithuania'),
-        array('mk', 'Macedonia'),
-        array('my', 'Malaysia'),
-        array('mt', 'Malta'),
-        array('mu', 'Mauritius'),
-        array('mx', 'Mexico'),
-        array('mn', 'Mongolia'),
-        array('mm', 'Myanmar'),
-        array('np', 'Nepal'),
-        array('nl', 'Netherlands'),
-        array('nz', 'New Zealand'),
-        array('ni', 'Nicaragua'),
-        array('ng', 'Nigeria'),
-        array('no', 'Norway'),
-        array('pk', 'Pakistan'),
-        array('pe', 'Peru'),
-        array('pl', 'Poland'),
-        array('pt', 'Portugal'),
-        array('ph', 'Republic of the Philippines'),
-        array('re', 'Reunion'),
-        array('ro', 'Romania'),
-        array('ru', 'Russia'),
-        array('sa', 'Saudi Arabia'),
-        array('sn', 'Senegal'),
-        array('sr-latn-rs', 'Serbia'),
-        array('rs', 'Serbia'),
-        array('sc', 'Seychelles'),
-        array('sg', 'Singapore'),
-        array('si', 'Slovenia'),
-        array('za', 'South Africa'),
-        array('es', 'Spain'),
-        array('lk', 'Sri Lanka'),
-        array('se', 'Sweden'),
-        array('ch', 'Switzerland'),
-        array('tw', 'Taiwan'),
-        array('th', 'Thailand'),
-        array('tt', 'Trinidad and Tobago'),
-        array('tr', 'Turkey'),
-        array('ae', 'United Arab Emirates'),
-        array('uk', 'United Kingdom'),
-        array('gb', 'United Kingdom'),
-        array('us',' United States'),
-        array('um', 'United States Minor Outlying Islands'),
-        array('ve', 'Venezuela'),
-        array('vn', 'Vietnam'),
-        array('ye', 'Yemen'),
-        array('yu', 'Yugoslavia'),
-        array('zw', 'Zimbabwe'),
-    );
-
     private $BingMapsKey = 'Ag0mGd-pnqRtYoueECNNzDNwARA_yN-agnPrcMzrVPsG3piMytyERDlXfkRhnmwQ';
 
+    private $baseURL = "http://dev.virtualearth.net/REST/v1/Locations/";
 
     /**
      * Display a listing of the resource.
@@ -127,11 +33,13 @@ class AddressController extends Controller
      */
     public function create()
     {
-        $countries = $this->countries;
-        $BingMapsKey = $this->BingMapsKey;
+        $data = [
+            'countries' => $this->countries,
+            'BingMapKey' => $this->BingMapsKey,
+            'title' => 'Add Your Location',
+        ];
 
-        // dd(count($countries));
-        return view('address.create', compact('countries', 'BingMapsKey'));
+        return view('address.create', $data);
     }
 
     /**
@@ -142,78 +50,43 @@ class AddressController extends Controller
      */
     public function store(Request $request)
     {
-        // dd( $_SERVER['HTTP_CLIENT_IP']);
+        /*
+        * Compose URI for Locations API request
+        *
+        * http://dev.virtualearth.net/REST/v1/Locations/countryRegion/adminDistrict/postalCode/locality/addressLine?key=yourBingMapsKey => with Address
+        *
+        * http://dev.virtualearth.net/REST/v1/Locations/{point}?key={BingMapsKey} => with latitude & longtitude
+        *
+        */
 
+        $findURL = $this->baseURL.$request['latitude'].','.$request['longitude']."?key=".$this->BingMapsKey;
 
-        // $ip = $request->ip(); /* Dynamic IP address */
-        // $ip = '162.159.24.227'; /* Static IP address */
-        // $currentUserInfo = Location::get('192.168.1.11');
-
-        $position = Location::get();
-        dd($position);
-
-        // $data = \Location::get($ipaddress);
-        // return view('details',compact('data'));
-
-
-        // May Use to get the nerest shop to customer
-        // $user_ip = getenv('REMOTE_ADDR');
-        // $geo = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$user_ip"));
-        // $country = $geo["geoplugin_countryName"];
-        // $city = $geo["geoplugin_city"];
-
-        // dd($geo);
-
-
-
-
-
-        // Get lat and long by address
-        // $BingMapsKey = $this->BingMapsKey;
-
-
-        // // URL of Bing Maps REST Services Locations API
-        // $baseURL = "http://dev.virtualearth.net/REST/v1/Locations";
-
-        // // Create variables for search parameters (encode all spaces by specifying '%20' in the URI)
-        // // http://dev.virtualearth.net/REST/v1/Locations/US/NY/10007/New York/291 Broadway?output=xml&key=yourKeyHere
-        // // http://dev.virtualearth.net/REST/v1/Locations/countryRegion/adminDistrict/postalCode/locality/addressLine?key=yourBingMapsKey
-
-        // $key = $BingMapsKey;
-        // $country = $request['Country_name']; // US
-        // $adminDistrict = str_ireplace(" ","%20",$request['State_name']); // NY
-        // $postalCode = str_ireplace(" ","%20",$request['zip_code']); // 10007
-        // $locality = str_ireplace(" ","%20",$request['City_name']); // New York
-        // $addressLine = str_ireplace(" ","%20",$request['street_name']); // 291 Broadway
-
-        // // Compose URI for Locations API request
-        // $findURL = $baseURL."/".$country."/".$adminDistrict."/".$postalCode."/".$locality."/".$addressLine."?output=xml&key=".$key;
-
-        // // get the response from the Locations API and store it in a string
-        // $output = file_get_contents($findURL);
-
+        // get the response from the Locations API and store it in a string
+        $output = file_get_contents($findURL);
 
         // $xmlObject = simplexml_load_string($output);
 
         // $json = json_encode($xmlObject);
-        // $phpArray = json_decode($json, true);
+        $phpArray = json_decode($output, true);
 
-        // dd($phpArray);
+        $addressDetails = $phpArray['resourceSets'][0]['resources'][0]['address'];
 
-        // dd($phpArray['ResourceSets']['ResourceSet']['Resources']['Location']['GeocodePoint'][0]);
+        $userAdd = Address::where('user_id', Auth::user()->id)->get();
 
+        if(count($userAdd) == 0) {
+            $address = new Address();
+        } else {
+            $address = Address::where('user_id', Auth::user()->id)->get()->first();
+        }
 
-        // array:4 [▼ // app\Http\Controllers\AddressController.php:167
-        //     "Latitude" => "40.714912"
-        //     "Longitude" => "-74.006017"
-        //     "CalculationMethod" => "Rooftop"
-        //     "UsageType" => "Display"
-        // ]
+            $address->user_id = Auth::user()->id;
+            $address->address_address = $addressDetails['formattedAddress'];
+            $address->address_latitude = $request['latitude'];
+            $address->address_longitude = $request['longitude'];
+            $address->address_ZipCode = '17444';
+            $address->save();
 
-
-        // abort_unless(\Gate::allows('company_create'), 403);
-        // $add = Address::create($request->all());
-        // return redirect()->route('dashboard');
+        return redirect()->back()->with('status', 'Added Successfully');
     }
 
     /**
--- resources/views/address/partials/form.blade.php
+++ resources/views/address/partials/form.blade.php
@@ -12,173 +12,84 @@
     <script type="text/javascript" src="https://ecn.dev.virtualearth.net/mapcontrol/mapcontrol.ashx?v=7.0"></script>
     <header>
         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-            {{ Route::currentRouteName() == 'customer.edit' ? __('Update Customer') : __('Create New Customer') }}
+            {{ __('Adding your Current Location') }}
         </h2>
-
-        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+        @if (session('status') === 'Added Successfully')
+            <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)"
+                class="text-sm text-gray-600 dark:text-gray-400">{{ __('Saved.') }}</p>
+        @endif
+        {{-- <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
             {{ __('Update or Add Customer Information.') }}
-        </p>
+        </p> --}}
     </header>
 
+    <form method="post" class="mt-6 space-y-6" action="{{ route('address.location.store') }}">
+        @csrf
 
-    <div onload="getLocation();">
-        <form method="post" class="mt-6 space-y-6 myForm" action="{{ route('address.location.store') }}">
-            @csrf
-
-            {{-- <div>
-                <iframe width="500" height="400" frameborder="0"
-                    src="https://www.bing.com/maps/embed?h=400&w=500&cp=30.045047094688513~31.290967969911776&lvl=13.413141983634729&typ=d&sty=h&src=SHELL&FORM=MBEDV8"
-                    scrolling="no">
-                </iframe>
-                <div style="white-space: nowrap; text-align: center; width: 500px; padding: 6px 0;">
-                    <a id="largeMapLink" target="_blank"
-                        href="https://www.bing.com/maps?cp=30.045047094688513~31.290967969911776&amp;sty=h&amp;lvl=13.413141983634729&amp;FORM=MBEDLD">View
-                        Larger Map</a> &nbsp; | &nbsp;
-                    <a id="dirMapLink" target="_blank"
-                        href="https://www.bing.com/maps/directions?cp=30.045047094688513~31.290967969911776&amp;sty=h&amp;lvl=13.413141983634729&amp;rtp=~pos.30.045047094688513_31.290967969911776____&amp;FORM=MBEDLD">Get
-                        Directions</a>
-                </div>
-            </div> --}}
+        {{-- latitude --}}
+        <div>
+            <x-text-input id="latitude" name="latitude" type="hidden" class="mt-1 block w-full" required autofocus
+                autocomplete="latitude" />
+        </div>
 
 
-            {{-- Country_name --}}
-            {{-- <div>
-                <x-input-label for="Country_name" :value="__('Country Name')" />
-                <select name="Country_name" id="Country_name"
-                    class="dark:border-gray-700 border-black-400 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block w-full">
-                    <option value="">-- {{ __('Chooce The Country') }} --</option>
-                    @foreach ($countries as $country)
-                        <option value="{{ $country[0] }}">{{ $country[1] }}</option>
-                    @endforeach
-                </select>
-                @error('Country_name')
-                    <div class="error text-red-500 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-300">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div> --}}
+        {{-- longitude --}}
+        <div>
+            <x-text-input id="longitude" name="longitude" type="hidden" class="mt-1 block w-full" required autofocus
+                autocomplete="longitude" />
+        </div>
 
-            {{-- State Name --}}
-            {{-- <div>
-                <x-input-label for="State_name" :value="__('State Name')" />
-                <x-text-input id="State_name" name="State_name" type="text" class="mt-1 block w-full" required autofocus
-                    autocomplete="State_name" />
-
-                @error('State_name')
-                    <div class="error text-red-500 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-300">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div> --}}
-
-            {{-- Zip Code --}}
-            {{-- <div>
-                <x-input-label for="zip_code" :value="__('Zip Code')" />
-                <x-text-input id="zip_code" name="zip_code" type="tel" class="mt-1 block w-full" required autofocus
-                    autocomplete="zip_code" />
-
-                @error('zip_code')
-                    <div class="error text-red-500 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-300">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div> --}}
-
-            {{-- City Name --}}
-            {{-- <div>
-                <x-input-label for="City_name" :value="__('City Name')" />
-                <x-text-input id="City_name" name="City_name" type="text" class="mt-1 block w-full" required autofocus
-                    autocomplete="City_name" />
-
-                @error('City_name')
-                    <div class="error text-red-500 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-300">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div> --}}
-
-            {{-- Street Name --}}
-            {{-- <div>
-                <x-input-label for="street_name" :value="__('Street Name')" />
-                <x-text-input id="street_name" name="street_name" type="text" class="mt-1 block w-full" required
-                    autofocus autocomplete="street_name" />
-
-                @error('street_name')
-                    <div class="error text-red-500 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-300">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div> --}}
-
-            {{-- <div>
-                <x-input-label for="latitude" :value="__('latitude')" />
-                <x-text-input id="latitude" name="latitude" type="hidden" class="mt-1 block w-full" required autofocus
-                    autocomplete="latitude" />
-
-                @error('latitude')
-                    <div class="error text-red-500 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-300">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-
-            <div>
-                <x-input-label for="longitude" :value="__('longitude')" />
-                <x-text-input id="longitude" name="longitude" type="hidden" class="mt-1 block w-full" required
-                    autofocus autocomplete="longitude" />
-
-                @error('longitude')
-                    <div class="error text-red-500 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-300">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div> --}}
+        <div class="flex items-center gap-4">
+            <x-primary-button>{{ __('Save') }}</x-primary-button>
+        </div>
 
 
-            <div>
-                <x-input-label for="longitude" :value="__('longitude')" />
-                <x-text-input id="longitude" name="longitude" type="text" class="mt-1 block w-full" required
-                    autofocus autocomplete="longitude" />
-
-                @error('longitude')
-                    <div class="error text-red-500 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-300">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-
-
-
-
-
-            <div class="flex items-center gap-4">
-                <x-primary-button>{{ __('Save') }}</x-primary-button>
-            </div>
-
-
-        </form>
-    </div>
-
-    {{-- <script>
-        function getLocation() {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(showPosition, showError);
-            }
-        }
-
-        function showPosition(position) {
-            document.querySelector('.myForm input[name="latitude"]').value = position.coords.latitude;
-            document.querySelector('.myForm input[name="longitude"]').value = position.coords.longitude;
-        }
-
-        function showError(error) {
-            switch (error.code) {
-                case error.PERMISION_DENIED:
-                    alert('You Must Allow the request for Geolocation to fill The Form');
-                    location.reload();
-                    break;
-            }
-        }
-    </script> --}}
+    </form>
 
 </section>
+
+
+<script>
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(a) {
+            // for (var t in a) {
+            //     // $('#general').append('<em>' + t + '</em> : ' + a[t] + '<br/>');
+            //     // alert(t + ': ' + a[t]);
+            //     console.console.log();
+            // }
+
+            // if (a.address) {
+            //     for (var t in a.address) {
+            //         // $('#address').append('<em>' + t + '</em> : ' + a.address[t] + '<br/>');
+            //         alert(t + ': ' + a.address[t]);
+
+            //     }
+            // }
+
+            if (a.coords) {
+                for (var t in a.coords) {
+                    // $('#coords').append('<em>' + t + '</em> : ' + a.coords[t] + '<br/>');
+                    // alert(t + ': ' + a.coords[t]);
+                    console.log(t + ': ' + a.coords[t]);
+                }
+
+                console.log(a.coords);
+                $('#longitude').val(a.coords.longitude);
+                $('#latitude').val(a.coords.latitude);
+
+                var point = new google.maps.LatLng(a.coords.latitude, a.coords.longitude);
+                new google.maps.Geocoder().geocode({
+                        'latLng': point
+                    },
+                    function(res, status) {
+                        var zip = res[0].formatted_address.match(/,\s\w{2}\s(\d{5})/);
+                        $("#zipCode").val(zip);
+                    }
+                );
+
+            }
+        });
+    } else {
+        alert('navigator.geolocation not supported.');
+    }
+</script>
